83065e10345794d4126f73f528578cd245697c4d
Matheus Freitas <bandeira.freitas2013@gmail.com>
2021-04-23 22:54:02 -0300

    Atualização tela de agendamento

--- Plano_de_vacinacao/front/src/Components/Form_Agendamento/index.jsx
+++ Plano_de_vacinacao/front/src/Components/Form_Agendamento/index.jsx
@@ -3,37 +3,38 @@ import { Button, Form, Container, Card } from "react-bootstrap";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-
 function Formulario() {
   const [dataNascimento, setdataNascimento] = useState(null);
   const [dataAgendamento, setdataAgendamento] = useState(null);
 
   return (
-
-
     <Container>
       <Card className="mt-5">
-        <Card.Header as="h5">Dados pessoais</Card.Header>
+        <Card.Header as="h5">Agendamento</Card.Header>
         <Card.Body>
           <Form>
+          <Card.Title>Dados pessoais:</Card.Title>
             <Form.Group controlId="formBasicEmail">
-              <Form.Label>Nome completo:</Form.Label>
+              <Form.Label>Nome completo</Form.Label>
               <Form.Control type="text" placeholder="Digite seu nome" />
             </Form.Group>
 
-            <Form.Label>Data de nascimento</Form.Label>
-            <br />
-            <DatePicker
-              selected={dataNascimento}
-              onChange={(date) => setdataNascimento(date)}
-              dateFormat="dd/MM/yyyy"
-              maxDate={new Date()}
-              showYearDropdown
-              scrollableMonthYearDropdown
-            />
+            <Form.Group>
+              <Form.Label>Data de nascimento</Form.Label>
+              <br />
+              <DatePicker
+                selected={dataNascimento}
+                onChange={(date) => setdataNascimento(date)}
+                dateFormat="dd/MM/yyyy"
+                maxDate={new Date()}
+                showYearDropdown
+                scrollableMonthYearDropdown
+              />
+            </Form.Group>
 
             <Card.Title className="mt-5">Agendamento:</Card.Title>
-            <Form.Group controlId="formBasicCheckbox">
+            <br />
+            <Form.Group>
               <Form.Label>Data do agendamento</Form.Label>
               <br />
               <DatePicker
@@ -44,11 +45,27 @@ function Formulario() {
               />
             </Form.Group>
 
-            
+            <Form.Group controlId="exampleForm.ControlSelect1">
+              <Form.Label>Horário</Form.Label>
+              <Form.Control as="select">
+                <option> </option>
+                <option>08:00</option>
+                <option>09:00</option>
+                <option>10:00</option>
+                <option>11:00</option>
+                <option>12:00</option>
+                <option>13:00</option>
+                <option>14:00</option>
+                <option>15:00</option>
+                <option>16:00</option>
+                <option>17:00</option>
+              </Form.Control>
+            </Form.Group>
 
             <Button variant="primary" type="submit">
               Submit
             </Button>
+
           </Form>
         </Card.Body>
       </Card>
@@ -57,5 +74,3 @@ function Formulario() {
 }
 
 export default Formulario;
-
-
